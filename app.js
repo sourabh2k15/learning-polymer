9386613830233c9d2d984b65bbf204aca4820a09
sourab <lostinmigration2k15@gmail.com>
2015-09-25 11:36:01 +0530

    babysteps

---
+++ app.js
@@ -0,0 +1,16 @@
+var http = require("http");
+var express = require("express");
+var app = express();
+var port = 5000;
+var server = http.Server(app);
+
+app.use(express.static(__dirname+'/public/'));
+
+server.listen(port,function(){
+    console.log("server listening on port "+port);
+});
+
+app.get('/handleget',function(req,res){
+    console.log("reponse get on /handleget");
+    res.send("hello from server!!");
+});
